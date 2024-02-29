51154b2aed931a7c55a80c413d8caddb9e47b33c
CodeChainWizard <shubhamdanecha789@gmail.com>
2024-02-29 21:05:56 +0530

    Create Header and Hero Seaction with animation and responsive

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,30 +1,81 @@
-import React from 'react'
-import {SiBlockchaindotcom} from "react-icons/si"
-import {CgMenuRight} from "react-icons/cg"
+import React from "react";
+import { SiBlockchaindotcom } from "react-icons/si";
+import { CgMenuRight } from "react-icons/cg";
 
-const Header = ({setActiveComponent, activeNetwork}) => {
+const Header = ({ setActiveComponent, activeNetwork }) => {
   const navMenu = [
-    "Home", "Liqudity", "Pool History", "Liqudity History", "Networks"
+    "Home",
+    "Liqudity",
+    "Pool History",
+    "Liqudity History",
+    "Networks",
   ];
   return (
     <header id="navbar-sticky" className="navbar">
-    <div className="container">
-      <nav className="">
-        <a className="logo" href="/">
-          <img src="assets/images/logo.png" alt="logo webAI" className="h-10"/>
-        </a>
+      <div className="container">
+        <nav className="">
+          <a className="logo" href="/">
+            <img
+              src="assets/images/logo.png"
+              alt="logo webAI"
+              className="h-[50px] w-[250px] -ml-10"
+            />
+          </a>
 
-        <div className='lg-hidden flex items-center ms-auto px-2.5'>
-          <button className='hs-collapse-toggle inline-flex items-center justify-center h-9 w-12 rounded-md border border-white/20 bg-default-100/5' type='button' id='hs-unstyled-collapse' data-hs-collaps="#mobileMenu" data-hs-type="collapse">
-            <CgMenuRight />
-          </button>
-        </div>
-       
-        
-      </nav>
-    </div>
-  </header>
-  )
-}
+          <div className="lg:hidden flex items-center ms-auto px-2.5">
+            <button
+              className="hs-collapse-toggle inline-flex items-center justify-center h-9 w-12 rounded-md border border-white/20 bg-default-100/5"
+              type="button"
+              id="hs-unstyled-collapse"
+              data-hs-collapse="#mobileMenu"
+              data-hs-type="collapse"
+            >
+              <CgMenuRight />
+            </button>
+          </div>
 
-export default Header
+          <div
+            id="mobileMenu"
+            className="hs-colllapse transition-all duration-300 lg:basis-auto basis-full grow hiiden lg:flex items-center justify-center mx-auto mt-2 lg:mt-0"
+          >
+            <ul id="navbar-navlist" className="navbar-nav">
+              {navMenu.map((list, index) => (
+                <li
+                  key={index}
+                  onClick={() => setActiveComponent(list)}
+                  className="nav-item"
+                >
+                  <a href="" className="nav-link">
+                    {list}
+                  </a>
+                </li>
+              ))}
+            </ul>
+
+            <div className="lg:hidden flex items-center pt-4 mt-4 lg:pt-0 lg:mt-0 border-t border-white/10 lg:border-none">
+              <a
+                href="#"
+                className="inline-flex items-center justify-center gap-2 bg-primary text-white py-2 px-6 rounded-full hover:bg-primary-hover transition-all duration-200"
+              >
+                <SiBlockchaindotcom />
+                {activeNetwork || "Select Network"}
+              </a>
+            </div>
+          </div>
+
+          <div className="hidden lg:flex items-center">
+            <a
+              href="#"
+              className="inline-flex items-center justify-center gap-2 bg-primary text-white py-2 px-6 rounded-full hover:bg-primary-hover transition-all duration-200"
+            >
+              <SiBlockchaindotcom />
+              {activeNetwork || "Select Network"}
+            </a>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- components/Home.jsx
+++ components/Home.jsx
@@ -1,7 +1,57 @@
+import { useState, useEffect } from "react";
 import React from "react";
+import { FaArrowRightLong } from "react-icons/fa6";
+import toast, { Toaster } from "react-hot-toast";
 
-const Home = () => {
-  return <div>Home</div>;
+// INTERNAL IMPORT
+import { IconOne, IconTwo } from "./index";
+
+const Home = ({ setActiveComponent, GET_POOL_DETAILS }) => {
+  // STATE VARIABLES
+  const [selectedNetwork, setSetselectedNetwork] = useState("");
+  const [poolAddress, setPoolAddress] = useState("");
+  const [poolDetails, setPoolDetails] = useState();
+
+  // NOTIFICATION
+  const notifyError = (msg) => {
+    toast.error(msg, { duration: 400 });
+  };
+
+  useEffect(() => {
+    const network = JSON.parse(localStorage.getItem("activeNetwork"));
+    setSetselectedNetwork(network);
+  }, []);
+
+  // CALLING FUNCTION
+  const CALL_POOL_ADDRESS = async (inputAddress) => {
+    const { rpcUrl } = selectedNetwork;
+
+    const zeroAddress = "0";
+
+    if (!inputAddress || !rpcUrl || inputAddress == zeroAddress) {
+      return notifyError("Provide Data or Invaild Address!!");
+    }
+
+    const poolDetails = await GET_POOL_DETAILS(inputAddress, selectedNetwork);
+    setPoolDetails(poolDetails);
+  };
+
+  return (
+    <section className="relative overflow-hidden pt-[72px] bg-default-950/40 backdrop-blur-3xl">
+      <div className="absolute h-14 w-14 bg-primary/10 top-2/3 start-80 -z-1 rounded-2xl rounded-tl-none rounded-br-none animate-[spin_10s_linear_infinite]"></div>
+
+      <div className="absolute h-14 w-14 bg-primary/10 -z-1 rounded-full animate-ping"></div>
+
+      <div className="px-6 pt-20 overflow-hidden">
+        <div className="relative">
+          <div className="absolute left-1/2 top-4 h-[1026px] w-[1026px] -translate-x-1/3 stroke-default-300/70 [mask-image:liner-gradient(to_bottom, white_20%, transparent_75%)] sm: top-16 -translate-x-1/2 lg:-top-16 lg:ml-12 xl:-top-14 xl:ml-0">
+            <IconOne />
+            <IconTwo />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
